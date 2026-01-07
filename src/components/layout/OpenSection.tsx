b29bff46c3a4645dd1705b5e4fc43cf92b9deab0
Rishabh Agnihotri <rishabh@flent.in>
2026-01-07 07:12:33 +0530

    fix(components): Allow OpenSection to accept all HTML attributes

--- src/components/layout/OpenSection.tsx
+++ src/components/layout/OpenSection.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-interface OpenSectionProps {
+interface OpenSectionProps extends React.HTMLAttributes<HTMLElement> {
     children: React.ReactNode;
-    className?: string;
-    id?: string;
-    style?: React.CSSProperties;
 }
 
-export const OpenSection = ({ children, className, id, style }: OpenSectionProps) => {
+export const OpenSection = ({ children, className, id, style, ...props }: OpenSectionProps) => {
     return (
         <section
             className={cn("relative w-full max-w-[100vw] mx-auto overflow-hidden", className)}
             id={id}
             style={style}
+            {...props}
         >
             {children}
         </section>
